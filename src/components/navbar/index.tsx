d9d1307bf9c7644b72d04b746526ad5e49497357
Danny Nguyen <dnguyen130@my.bcit.ca>
2023-07-04 19:17:12 -0700

    header progress

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,4 +1,12 @@
 import { ReactElement } from "react";
+import { IoStorefront } from "react-icons/io5";
+import { CgProfile } from "react-icons/cg";
+import { BsCart3 } from "react-icons/bs";
+
+interface PrimaryLinkType {
+  title: string;
+  icon: JSX.Element;
+}
 
 const SecondaryLinks: string[] = [
   "Order Status",
@@ -6,6 +14,21 @@ const SecondaryLinks: string[] = [
   "Best Buy Business",
   "Français",
 ];
+
+const PrimaryLinks: Record<string, PrimaryLinkType> = {
+  stores: {
+    title: "Stores",
+    icon: <IoStorefront size="100%" />,
+  },
+  account: {
+    title: "Account",
+    icon: <CgProfile size="100%" />,
+  },
+  cart: {
+    title: "Cart",
+    icon: <BsCart3 size="100%" />,
+  },
+};
 
 export default function Navbar(): ReactElement {
   return (
@@ -16,9 +39,22 @@ export default function Navbar(): ReactElement {
             return <li>{o}</li>;
           })}
         </ul>
-        <a aria-label="Best Buy Clone" href="/" target="_self">
-          <img src="bestbuy_logo.svg" width="82" height="48" />
-        </a>
+        <div className="header-mainrow">
+          <a aria-label="Best Buy Clone" href="/" target="_self">
+            <img src="bestbuy_logo.svg" width="82" height="48" />
+          </a>
+          <div className="searchbar"></div>
+          <div className="primarylink-container">
+            {Object.values(PrimaryLinks).map((o) => {
+              return (
+                <div className="primarylink">
+                  <div className="primarylink-icon">{o.icon}</div>
+                  <div className="primarylink-title">{o.title}</div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
       </div>
     </header>
   );
