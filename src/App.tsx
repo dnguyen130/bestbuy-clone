c6b261d6c0c484ec0e9334a10eeec4a821d3bc0b
dnguyen130 <dnguyen130@my.bcit.ca>
2023-07-07 15:35:54 -0700

    starting main

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import Navbar from "./components/navbar";
 
 function App() {
   return (
-    <>
+    <div className="AppContainer">
       <Navbar />
-    </>
+      <main></main>
+    </div>
   );
 }
 
